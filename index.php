e4a69aca6c72e5a36470a67e089b5ce808cd211b
AzureAD\佐野裕 <ysano@hb-corp.jp>
2020-08-24 15:19:06 +0900

    佐野自己紹介追加。

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     <body>
       <?php echo "Hello World! php" ?>
       <ul>
-        <li>sano</li>
+        <li>自己紹介</li>
+        <li>佐野。ハートビーツ社に今年7月入社。インフラエンジニア。現在は開発部門と人材育成部門のシニアマネージャー。Kpopと韓国好き。</li>
       </ul>
     </body>
   </html>
