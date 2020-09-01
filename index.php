7ab1521f45b5c8cad11ae4a4b192b9c543cc79f7
AzureAD\佐野裕 <ysano@hb-corp.jp>
2020-09-01 19:31:48 +0900

    文字化けの修正

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
     <body>
       <?php echo "Hello World! php" ?>
       <ul>
-        <li>���ȏЉ�</li>
-        <li>����B�n�[�g�r�[�c�Ђɍ��N7�����ЁB�C���t���G���W�j�A�B���݂͊J������Ɛl�ވ琬����̃V�j�A�}�l�[�W���[�BKpop�Ɗ؍��D���B</li>
+        <li>自己紹介</li>
+        <li>佐野。ハートビーツ社に今年7月入社。インフラエンジニア。現在は開発部門と人材育成部門のシニアマネージャー。Kpopと韓国好き。</li>
+        <li>安田。</li>
+        <li>泉田。</li>
+        <li>高梨。</li>
       </ul>
+      <ul>
+      </ul>
+
     </body>
   </html>
